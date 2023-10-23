3a4293278190bc7cc7df6c33f37e6fcd7fcab60b
Eliseo <Eliseo@LAPTOP-OIA8MLF7>
2023-10-23 08:29:47 +0700

    Add project files.

---
+++ nobleAreaProject/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int length;
+        int width;
+
+        int area;
+
+        Console.Write("Enter length: ");
+        length = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter width: ");
+        width = int.Parse(Console.ReadLine());
+
+        area = (length * width);
+
+        Console.WriteLine($"area: {area}");
+    }
+}
